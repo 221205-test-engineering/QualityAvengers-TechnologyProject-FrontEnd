a700b31b20ced58574e012c3198a9ae48404364b
Soni Mohandas <sonireetha@gmail.com>
2023-01-13 16:48:21 -0500

    renamed directories

---
+++ src/test/java/com/revature/admin/pages/AdminScheduleGamesPage.java
@@ -0,0 +1,33 @@
+package com.revature.admin.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+public class AdminScheduleGamesPage {
+    public WebDriver driver;
+    public AdminScheduleGamesPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    @FindBy(id = "venueOptions")
+    public List<WebElement> venueList;
+    @FindBy(xpath = "//input[@name = 'time']")
+    public WebElement time;
+    @FindBy(id = "seasonOptions")
+    public List<WebElement> seasonOptions;
+    @FindBy(name = "sport")
+    public List<WebElement> sportList;
+    @FindBy(id = "homeOptions")
+    public List<WebElement> homeTeamList;
+    @FindBy(id = "awayOptions")
+    public List<WebElement> awayTeamList;
+    @FindBy(xpath = "//button[text() = 'Schedule']")
+    public WebElement schedule;
+    @FindBy(xpath = "//a[text() = 'Back']")
+    public WebElement scheduleToAdminPage;
+}
---
+++ src/test/java/com/revature/admin/runner/AdminRunner.java
@@ -0,0 +1,51 @@
+package com.revature.admin.runner;
+
+
+
+import com.revature.admin.pages.*;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/com/revature/admin/features", glue = "com.revature.admin.steps")
+public class AdminRunner {
+
+        public static WebDriver driver;
+        public static LoginPage login;
+        public static AdminPage admin;
+        public static AdminViewsGamePage viewGames;
+        public static AdminViewsVenuePage venues;
+        public static AdminViewsSeasonPage seasons;
+        public static EditUserProfilePage editProfile;
+        public static ManageUserRolePage userRoles;
+        public static AddSeasonPage addSeason;
+        public static AdminScheduleGamesPage scheduleGames;
+        @BeforeClass
+        public static void setup(){
+            WebDriverManager.chromedriver().setup();  // Driver setup
+            driver = new ChromeDriver();  // Instantiating chrome driver
+            login = new LoginPage(driver);
+            admin = new AdminPage(driver);
+            viewGames = new AdminViewsGamePage(driver);
+            venues = new AdminViewsVenuePage(driver);
+            seasons = new AdminViewsSeasonPage(driver);
+            editProfile = new EditUserProfilePage(driver);
+            userRoles = new ManageUserRolePage(driver);
+            addSeason = new AddSeasonPage(driver);
+            scheduleGames = new AdminScheduleGamesPage(driver);
+        }
+
+        @AfterClass
+        public static void teardown()
+        {
+            driver.quit();
+        }
+}
+
