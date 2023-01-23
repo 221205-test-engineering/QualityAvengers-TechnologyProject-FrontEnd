f36225d24a69ea28e0bc7c6a64bc0f5cfdd8e2bf
khavvia <khavvia@gmail.com>
2023-01-23 12:51:04 -0600

    added a feature, having issues with an old feature that was working before

---
+++ src/test/java/com/revature/steps/khavvia/LoginNegativeStepImpl.java
@@ -0,0 +1,22 @@
+package com.revature.steps.khavvia;
+
+import com.revature.pages.IndexPage;
+import com.revature.pages.LoginPage;
+import com.revature.pages.MainPage;
+import com.revature.runners.LoginUpdateRegisterRunner;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.Assert.assertEquals;
+
+public class LoginNegativeStepImpl {
+    public WebDriver driver1 = LoginUpdateRegisterRunner.driver1;
+    public IndexPage indexPage1 = LoginUpdateRegisterRunner.indexPage1;
+    public LoginPage loginPage1 = LoginUpdateRegisterRunner.loginPage1;
+    public MainPage mainPage1 = LoginUpdateRegisterRunner.mainPage1;
+
+    @Then("the employee should see an alert saying user does not exist")
+    public void the_employee_should_see_an_alert_saying_user_does_not_exist() {
+        assertEquals("User does not exist", driver1.switchTo().alert().getText());
+    }
+}
